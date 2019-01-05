b43cde95c5961068bebfeb766cc31504e75c15a1
unknown <alexandre.mary.adenni@gmail.com>
2019-01-05 12:46:16 +0100

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,73 @@
 var index = 0;
+var totalSlides = 2;
+var translateMover = 0;
 var arrowRight = document.querySelectorAll(".project__arrow--right");
 var arrowLeft = document.querySelectorAll(".project__arrow--left");
 var mover = document.getElementById("mover");
+var wikipediaSvg = document.getElementById("wikipediaSvg");
+var bearMouth = document.getElementById("bear--mouth");
+var bearHead = document.getElementById("bear--head");
 
-for (let i = 0; i < arrowRight.length; i++) {
-  arrowRight[i].addEventListener('click', next);
+addListener();
+function addListener() {
+  console.log('add');
+  for (let i = 0; i < arrowRight.length; i++) {
+    arrowRight[i].addEventListener("click", next);
+  }
+  for (let i = 0; i < arrowRight.length; i++) {
+    arrowLeft[i].addEventListener("click", back);
+  }
 }
 
-for (let i = 0; i < arrowRight.length; i++) {
-  arrowLeft[i].addEventListener('click', back);
+function removeListener() {
+  console.log("remove");
+  for (let i = 0; i < arrowRight.length; i++) {
+    arrowRight[i].removeEventListener("click", next);
+  }
+  for (let i = 0; i < arrowRight.length; i++) {
+    arrowLeft[i].removeEventListener("click", back);
+  }
 }
 
-function next(){
- mover.style.transform = "translateY(-100vh)";
+function next() {
+  leave();
+  setTimeout(function() {
+    if (index < totalSlides - 1) {
+      translateMover = translateMover - 100;
+      mover.style.transform = "translateY(" + translateMover + "vh)";
+      index++;
+    } else {
+      translateMover = 0;
+      mover.style.transform = "translateY(" + translateMover + "vh)";
+      index = 0;
+    }
+    addListener();
+  }, 2000);
 }
 
 function back() {
-  mover.style.transform = "translateY(0vh)";
+  leave();
+  setTimeout(function() {
+  if (index > 0) {
+    translateMover = translateMover + 100;
+    mover.style.transform = "translateY(" + translateMover + "vh)";
+    index--;
+  } else {
+    translateMover = (totalSlides - 1) * -100;
+    mover.style.transform = "translateY(" + translateMover + "vh)";
+    index = 1;
+  }
+  addListener();
+}, 2000);
+}
+
+function leave() {
+  removeListener();
+  if (index === 0) {
+    wikipediaSvg.classList.add("leave");
+  }
+  if (index === 1) {
+    bearMouth.classList.add("leave");
+    bearHead.classList.add("leave");
+  }
 }
