b10a12afe3aace9ee7162851f41f08b9b61d88f4
unknown <alexandre.mary.adenni@gmail.com>
2019-01-16 15:34:23 +0100

    update

--- src/js/menu.js
+++ src/js/menu.js
@@ -10,7 +10,23 @@ works.addEventListener("click", function() {
 // Scroll header - Nespresso color
 window.onscroll = () => {
   const nav = document.querySelector(".header--nespresso");
-  if (window.scrollY <= 90) nav.className = "header header--nespresso";
+  if (window.scrollY <= 100) nav.className = "header header--nespresso";
   else nav.className = "header header--nespresso isScrolled";
+};
+//
+
+// Scroll header - wiki color
+window.onscroll = () => {
+  var navWiki = document.querySelector(".header--wikipedia");
+  if (window.scrollY <= 100) navWiki.className = "header header--wikipedia";
+  else navWiki.className = "header header--wikipedia isScrolled";
+};
+//
+
+// Scroll header - moschino color
+window.onscroll = () => {
+  const navMoschino = document.querySelector(".header--moschino");
+  if (window.scrollY <= 100) navMoschino.className = "header header--moschino";
+  else navMoschino.className = "header header--moschino isScrolled";
 };
 //
