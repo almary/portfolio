a4c41f0e106608a3cb502be9d85e47c2a25f8ae6
unknown <alexandre.mary.adenni@gmail.com>
2019-01-16 15:00:17 +0100

    update

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,16 @@
 var menu = document.getElementById("headerMenu");
 var works = document.getElementById("headerWorks");
 
-
 // Menu
-
 works.addEventListener("click", function() {
   menu.classList.toggle("isClicked");
 });
+//
 
+// Scroll header - Nespresso color
+window.onscroll = () => {
+  const nav = document.querySelector(".header--nespresso");
+  if (window.scrollY <= 90) nav.className = "header header--nespresso";
+  else nav.className = "header header--nespresso isScrolled";
+};
 //
