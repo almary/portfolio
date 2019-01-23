511b723696e50156f3c37d489bc93ffacff37b88
unknown <alexandre.mary.adenni@gmail.com>
2019-01-23 12:28:22 +0100

    update

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,5 +1,6 @@
 var cursor = document.getElementById("cursor");
 var lastPosition = 0;
+var links = document.querySelectorAll("a");
 
 window.addEventListener('scroll', function(e) {
  lastPosition = window.scrollY;
@@ -12,3 +13,27 @@ function mouseMove(e) {
   cursor.style.top = -20 + e.clientY + lastPosition + "px";
 }
 
+// Click animation
+
+document.addEventListener("click", mouseClick);
+
+function mouseClick() {
+  cursor.classList.add("isClicked");
+  setTimeout(() => {
+    cursor.classList.remove("isClicked");
+  }, 100);
+}
+
+// Hover animation
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseover", function() {
+    cursor.classList.add("hover");
+  });
+}
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("mouseout", function() {
+    cursor.classList.remove("hover");
+  });
+}
