ceb4a2b923b2dad3cbd9a421ad5c119261420394
unknown <alexandre.mary.adenni@gmail.com>
2019-01-30 12:27:42 +0100

    update

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -2,13 +2,17 @@ var cursor = document.getElementById("cursor");
 var lastPosition = 0;
 var links = document.querySelectorAll("a");
 
-window.addEventListener('scroll', function(e) {
- lastPosition = window.scrollY;
-});
+window.addEventListener("scroll", scrollUpdate);
+
+function scrollUpdate(e) {
+  lastPosition = window.scrollY;
+  cursor.style.top = -20 + lastPosition + position + "px";
+}
 
 document.addEventListener("mousemove", mouseMove);
 
 function mouseMove(e) {
+  position = e.clientY;
   cursor.style.left = -20 + e.clientX + "px";
   cursor.style.top = -20 + e.clientY + lastPosition + "px";
 }
