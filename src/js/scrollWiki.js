b125a2142332753772f553c6f1772b991d2ea2a3
unknown <alexandre.mary.adenni@gmail.com>
2019-02-02 13:52:42 +0100

    update

--- src/js/scrollWiki.js
+++ src/js/scrollWiki.js
@@ -9,3 +9,21 @@ window.onscroll = () => {
 // // Parallax init
 // let Rellax = require('rellax');
 // let rellax = new Rellax('.rellax');
+
+
+// Progress Bar
+var bar = document.querySelector(".progress");
+console.log(bar)
+
+document.onscroll = function(){
+  var pos = showPosition(document.body);
+  bar.style.width = pos + "%";
+  console.log(pos);
+};
+
+function showPosition(elm){
+  var parent = elm.parentNode,
+      pos = (elm.scrollTop || parent.scrollTop) / (parent.scrollHeight - parent.clientHeight ) * 100;
+
+  return pos;
+};
