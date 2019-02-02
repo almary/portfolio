06439104519893463bcc40ef20a2a69418cf9e6f
unknown <alexandre.mary.adenni@gmail.com>
2019-02-02 15:12:18 +0100

    update

--- src/js/scrollWiki.js
+++ src/js/scrollWiki.js
@@ -6,19 +6,12 @@ window.onscroll = () => {
 };
 //
 
-// // Parallax init
-// let Rellax = require('rellax');
-// let rellax = new Rellax('.rellax');
-
-
 // Progress Bar
 var bar = document.querySelector(".progress");
-console.log(bar)
 
 document.onscroll = function(){
   var pos = showPosition(document.body);
   bar.style.width = pos + "%";
-  console.log(pos);
 };
 
 function showPosition(elm){
