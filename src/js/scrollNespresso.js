3d87b119e876fd68f2eb7899ef1602d48ad4ac3c
unknown <alexandre.mary.adenni@gmail.com>
2019-02-06 10:24:52 +0100

    update

--- src/js/scrollNespresso.js
+++
@@ -1,7 +0,0 @@
-// Scroll header - Nespresso color
-window.onscroll = () => {
-  const nav = document.querySelector(".header--nespresso");
-  if (window.scrollY <= 100) nav.className = "header header--nespresso";
-  else nav.className = "header header--nespresso isScrolled";
-};
-//
