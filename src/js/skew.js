1561f6f852e97f811627b4ccda3462a84863744f
unknown <alexandre.mary.adenni@gmail.com>
2019-02-06 15:36:47 +0100

    update

--- src/js/skew.js
+++ src/js/skew.js
@@ -1,29 +1,51 @@
-let images = document.querySelectorAll("img");
-let videos = document.querySelectorAll("video");
-let text = document.querySelectorAll(".description__text");
-let currentPixel = window.pageYOffset;
+// let images = document.querySelectorAll("img");
+// let videos = document.querySelectorAll("video");
+// let text = document.querySelectorAll(".description__text");
+// let currentPixel = window.pageYOffset;
 
-if (window.matchMedia("(min-width: 750px)").matches) {
-  const looper = function() {
-    const newPixel = window.pageYOffset;
-    var diff = newPixel - currentPixel;
-    var speed = diff * 0.1;
-    for (let i = 0; i < images.length; i++) {
-      images[i].style.transform = "skewY(" + speed + "deg)";
-    }
+// if (window.matchMedia("(min-width: 750px)").matches) {
+//   const looper = function() {
+//     const newPixel = window.pageYOffset;
+//     var diff = newPixel - currentPixel;
+//     var speed = diff * 0.1;
+//     for (let i = 0; i < images.length; i++) {
+//       images[i].style.transform = "skewY(" + speed + "deg)";
+//     }
 
-    for (let i = 0; i < videos.length; i++) {
-      videos[i].style.transform = "skewY(" + speed + "deg)";
-    }
+//     for (let i = 0; i < videos.length; i++) {
+//       videos[i].style.transform = "skewY(" + speed + "deg)";
+//     }
 
-    for (let i = 0; i < text.length; i++) {
-      text[i].style.transform = "skewY(" + speed + "deg)";
-    }
+//     for (let i = 0; i < text.length; i++) {
+//       text[i].style.transform = "skewY(" + speed + "deg)";
+//     }
 
-    currentPixel = newPixel;
+//     currentPixel = newPixel;
 
-    requestAnimationFrame(looper);
-  };
+//     requestAnimationFrame(looper);
+//   };
 
-  looper();
+//   looper();
+// }
+
+var scrollContainer = document.querySelector('.scroll-container')
+var scrollContent = document.querySelector('.scroll-content')
+
+function resize () {
+  document.body.style.height = scrollContent.offsetHeight + 'px'
 }
+
+var scrollY = 0
+
+function tick () {
+  requestAnimationFrame(tick)
+  var diff = (scrollY - document.documentElement.scrollTop)
+  scrollY -= (scrollY - document.documentElement.scrollTop) * 0.15
+  scrollContent.style.transform = 'translate3d(0, ' + -scrollY + 'px, 0) skew(0, '+diff*.009+'deg)'
+}
+
+tick()
+
+resize()
+
+window.addEventListener('resize', resize)
